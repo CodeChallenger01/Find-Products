cd8114bde1b7674e9186225f58a50fac8cabcc71
Manish Mishra <manish.mishra@knoldus.com>
2023-03-06 12:59:51 +0530

    Try catch block

--- find-product/build.sbt
+++ find-product/build.sbt
@@ -2,6 +2,9 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.10"
 
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.15" % "Test"
+libraryDependencies += "org.scalatest" %% "scalatest-funsuite" % "3.2.15" % "test"
+
 lazy val root = (project in file("."))
   .settings(
     name := "find-product"
--- find-product/src/main/scala/Main.scala
+++ find-product/src/main/scala/Main.scala
@@ -1,14 +1,22 @@
 class FindProduct {
-  val arr:Array[Int]= Array(1, 2, 3, 4, 5)
-  var result= 1
+  val arr: Array[Int] = Array(1, 2, 3, 4, 5)
+  var result = 1
 
   //method to product the element of array
   def product(){
     for(a<-arr){
-      result = result*a
+      try{
+        result = result * a
+      }
+      catch{
+        case x: ArithmeticException=>{
+          print("Error")
+        }
+      }
     }
     println("Product of element of Array is :" +result)
   }
+
 }
 
 //Singleton Object
